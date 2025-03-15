cf780373e8272e0e4db23885960a1a94669c8b34
Syed Affan Hameed <syedaffanhameed@gmail.com>
2025-03-15 16:55:20 +0530

    three uses of the responses api

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+// const response = await openai.responses.create({
+//     model:"gpt-4o-mini",
+//     input:"answer me breifly, one single tip to improve my mental health and well-being",
+// });
+
+//const response = await openai.responses.create({
+//     model:"gpt-4o-mini",
+//     input:[
+//         {
+//             role:"user",
+//             content:"describe the image?",
+//         },
+//         {
+//             role:"user",
+//             content:[
+//                 {
+//                     type:"input_image",
+//                     image_url:"https://images.unsplash.com/photo-1719404363999-5145022af635?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y3Jpc3RpYW5vJTIwcm9uYWxkb3xlbnwwfHwwfHx8MA%3D%3D"
+//                 }
+//             ]
+//         }
+//     ]
+// })
+
+// console.log(response.output_text);
+
+const stream = await openai.responses.create({
+    model: "gpt-4o",
+    input: [
+        {
+            role: "user",
+            content: "were egyptians in contact with aliens?",
+        },
+    ],
+    stream: true,
+});
+
+for await (const event of stream) {
+console.log(event)
+}
